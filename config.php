cfcd0dae851b18875a62fb634d38fe3d293e2d11
LionRouge1 <matchoudiavlessi@gmail.com>
2025-04-11 13:04:21 +0000

    Require environment variables

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
-$host = '127.0.0.1'; // or 'localhost'
-$db   = 'nexora';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_CHARSET']);
+
+// Access variables
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$charset = $_ENV['DB_CHARSET'];
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
@@ -13,9 +20,9 @@ $options = [
     PDO::ATTR_EMULATE_PREPARES   => false,                  // use real prepared statements
 ];
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    echo "✅ Connection successful!";
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+// try {
+//     $pdo = new PDO($dsn, $user, $pass, $options);
+//     echo "✅ Connection successful!";
+// } catch (\PDOException $e) {
+//     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+// }
