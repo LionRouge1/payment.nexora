4d56f23e7a8322fde8e32534a766db2996d1b934
LionRouge1 <matchoudiavlessi@gmail.com>
2025-04-15 22:23:56 +0000

    Remove unuse controllers

--- src/Controllers/WebsiteController.php
+++ src/Controllers/WebsiteController.php
@@ -13,8 +13,12 @@ class WebsiteController extends ApplicationController
 
     public function search()
     {
-        $domain = $_POST['domain'] ?? null;
         $error = null;
+        if(!isset($_POST['domain']) || empty($_POST['domain'])) {
+            $error = 'Domain field cannot be empty.';
+        }
+
+        $domain = $_POST['domain'] ?? null;
         $website = null;
         $author = null;
         if ($domain) {
@@ -29,25 +33,38 @@ class WebsiteController extends ApplicationController
             $website = null;
         }
 
+        $website_id = $website ? $website->getId() : null;
+        $email = $author ? $author->getEmail() : null;
+        $fullname = $author ? $author->getFullname() : null;
+        $name = $website ? $website->getName() : null;
+        $domain = $website ? $website->getDomain() : null;
+        $author_id = $author ? $author->getId() : null;
+
         $data = [
-          'fullname' => $author ? $author->getFullname() : null,
+          'fullname' => $fullname,
           'error' => $error,
-          'domain' => $website ? $website->getDomain() : null,
-          'name' => $website ? $website->getName() : null,
-          'email' => $author ? $author->getEmail() : null,
-          'website_id' => $website ? $website->getId() : null,
+          'domain' => $domain,
+          'name' => $name,
+          'email' => $email,
+          'website_id' => $website_id,
         ];
+        $_SESSION['website_id'] = $website_id;
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['domain'] = $domain;
+        $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['author_id'] = $author_id;
 
         $this->render('websites/index', compact('data'));
     }
 
-    public function contact()
-    {
-        $this->render('website/contact');
-    }
+    // public function contact()
+    // {
+    //     $this->render('website/contact');
+    // }
 
-    public function services()
-    {
-        $this->render('website/services');
-    }
+    // public function services()
+    // {
+    //     $this->render('website/services');
+    // }
 }
