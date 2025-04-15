37813bf277cff51ee3e534499d94437d0c4b5f27
LionRouge1 <matchoudiavlessi@gmail.com>
2025-04-15 22:43:45 +0000

    Change the key to use environment variable

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,48 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
   const paymentBtn = document.getElementById('payment-button')
-  const flipBox = document.getElementById("flipBox");
+  const flipBox = document.getElementById('flipBox')
 
-  if(flipBox) {
+  if (flipBox) {
     setInterval(() => {
-      flipBox.classList.toggle("flipped");
-    }, 5000);
+      flipBox.classList.toggle('flipped')
+    }, 5000)
   }
 
   if (paymentBtn) {
-    paymentBtn.addEventListener('click', async() => {
+    paymentBtn.addEventListener('click', async () => {
       const response = await fetch('/payment.nexora/public/initialize', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         }
-      });
+      })
 
-      const data = await response.json();
-      const popup = new PaystackPop();
+      const data = await response.json()
+      const popup = new PaystackPop()
       popup.newTransaction({
         email: data.email,
         amount: data.amount,
-        metadata: {website_id: data.website_id},
-        key: 'pk_test_d9b121d9c7e8280494f210cda9558515b08827be',
-        onSuccess: async(transaction) => {
-          // console.log(transaction)
-            const request = await fetch('/payment.nexora/public/verify', {
+        metadata: { website_id: data.website_id },
+        key: process.env.PAYSTACK_PK || '',
+        onSuccess: async transaction => {
+          const request = await fetch('/payment.nexora/public/verify', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: new URLSearchParams({
               reference: transaction.reference,
-              _method: 'POST',
+              _method: 'POST'
             }).toString()
-            });
+          })
 
-          const response = await request.json();
+          const response = await request.json()
           if (response.status === 'success') {
             // Handle successful payment
-            window.location.href = '/payment.nexora/public/payment/thanks';
+            window.location.href = '/payment.nexora/public/payment/thanks'
           } else {
-            console.log('Payment failed', response);
+            console.log('Payment failed', response)
           }
         },
         onLoad: response => {
@@ -53,34 +52,34 @@ document.addEventListener('DOMContentLoaded', () => {
           const request = await fetch('/payment.nexora/public/verify', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: new URLSearchParams({
               error: 'Transaction not completed. Kindly try again.',
-              _method: 'POST',
+              _method: 'POST'
             }).toString()
-          });
+          })
 
-          if(request.status === 500) {
+          if (request.status === 500) {
             // Handle successful error
-            
-            window.location.reload(true);
+
+            window.location.reload(true)
           }
         },
-        onError: async(error) => {
+        onError: async error => {
           const request = await fetch('/payment.nexora/public/verify', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: new URLSearchParams({
               error: error.message,
-              _method: 'POST',
+              _method: 'POST'
             }).toString()
-          });
-          if(request.status === 500) {
+          })
+          if (request.status === 500) {
             // Handle successful error
-            window.location.reload(true);
+            window.location.reload(true)
           }
         }
       })
