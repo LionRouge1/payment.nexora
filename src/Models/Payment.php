e7165c718ec9da939622bf76bed167c7661f3d9b
LionRouge1 <matchoudiavlessi@gmail.com>
2025-04-17 02:36:43 +0000

    Clean up the model

--- src/Models/Payment.php
+++ src/Models/Payment.php
@@ -1,5 +1,4 @@
 <?php
-// require_once 'modules.php';
 namespace App\Models;
 use App\Config\Database;
 use PDO;
@@ -9,9 +8,9 @@ use App\Models\Website;
 use App\Models\Author;
 
 class Payment extends ApplicationRecord {
-  // private $id, $website_id, $author_id, $amount, $payment_method, $status, 
-  //         $reference, $currency, $payment_id, $receipt_number, $ip_address, 
-  //         $updated_at, $created_at;
+  protected $id, $website_id, $author_id, $amount, $payment_method, $status, 
+          $reference, $currency, $payment_id, $receipt_number, $ip_address, 
+          $updated_at, $created_at;
 
   public function __construct($data) {
     parent::__construct();
@@ -22,75 +21,10 @@ class Payment extends ApplicationRecord {
         }
       }
     } else {
-      // If an ID is provided, load the payment details
       $this->id = $data;
       $this->load($data);
     }
   }
-
-  // Getters
-  // public function __call($name, $arguments) {
-  //   $property = strtolower(preg_replace('/^get/', '', $name));
-  //   if (property_exists($this, $property)) {
-  //     return $this->$property;
-  //   }
-  //   throw new Exception("Method $name does not exist");
-  // }
-
-  // public static function find($id) {
-  //   return new Payment($id);
-  // }
-
-  // public static function all() {
-  //   $db = Database::getInstance();
-  //   $stmt = $db->prepare("SELECT * FROM payments");
-  //   $stmt->execute();
-  //   $payments = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  //   $paymentObjects = [];
-  //   foreach ($payments as $payment) {
-  //     $paymentObjects[] = new Payment($payment);
-  //   }
-  //   return $paymentObjects;
-  // }
-
-  // public static function create($website_id, $author_id, $amount, $payment_id, $currency, $receipt_number, $ip_address, $status, $reference, $payment_method) {
-  //   $db = Database::getInstance();
-  //   $stmt = $db->prepare("INSERT INTO payments (payment_id, amount, currency, payment_method, reference, status, receipt_number, ip_address, website_id, author_id) VALUES ( :payment_id, :amount, :currency, :payment_method, :reference, :status, :receipt_number, :ip_address, :website_id, :author_id)");
-  //   $stmt->bindParam(':website_id', $website_id, PDO::PARAM_INT);
-  //   $stmt->bindParam(':author_id', $author_id, PDO::PARAM_INT);
-  //   $amount = $amount / 100;
-  //   $stmt->bindParam(':amount', $amount);
-  //   $stmt->bindParam(':status', $status, PDO::PARAM_INT);
-  //   $stmt->bindParam(':reference', $reference);
-  //   $stmt->bindParam(':method', $payment_method);
-  //   $stmt->bindParam(':currency', $currency);
-  //   $stmt->bindParam(':receipt_number', $receipt_number);
-  //   $stmt->bindParam(':ip_address', $ip_address);
-  //   $stmt->bindParam(':payment_id', $payment_id);
-  //   $stmt->execute();
-  //   return self::find($db->lastInsertId());
-  // }
-
-  // public function loadPayment() {
-  //   $stmt = $this->db->prepare("SELECT * FROM payments WHERE id = :id");
-  //   $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
-  //   $stmt->execute();
-  //   $payment = $stmt->fetch(PDO::FETCH_ASSOC);
-  //   if ($payment) {
-  //     $this->website_id = $payment['website_id'];
-  //     $this->amount = $payment['amount'];
-  //     $this->status = $payment['status'];
-  //     $this->author_id = $payment['author_id'];
-  //     $this->reference = $payment['reference'];
-  //     $this->currency = $payment['currency'];
-  //     $this->payment_id = $payment['payment_id'];
-  //     $this->payment_method = $payment['payment_method'];
-  //     $this->receipt_number = $payment['receipt_number'];
-  //     $this->ip_address = $payment['ip_address'];
-  //     $this->updated_at = $payment['updated_at'];
-  //     $this->created_at = $payment['created_at'];
-  //   }
-  // }
 
   public function author() {
     $stmt = $this->db->prepare("SELECT * FROM authors WHERE id = :author_id");
@@ -124,5 +58,4 @@ class Payment extends ApplicationRecord {
     }
     return null;
   }
-
 }
