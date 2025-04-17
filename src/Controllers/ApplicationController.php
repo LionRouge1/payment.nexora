edd934d2d1a1db90eb48e9fda6742f79f44335c5
LionRouge1 <matchoudiavlessi@gmail.com>
2025-04-17 03:14:11 +0000

    Make layout accessible for children

--- src/Controllers/ApplicationController.php
+++ src/Controllers/ApplicationController.php
@@ -3,31 +3,32 @@ namespace App\Controllers;
 
 class ApplicationController
 {
-    // protected $db;
+  protected $layout = 'main';
+  private $layoutPath;
 
-    // public function __construct()
-    // {
-    //     $this->db = \App\Config\Database::getInstance();
-    // }
+  public function __construct()
+  {
+    $this->layoutPath = __DIR__ . '/../Views/layouts/' . $this->layout . '.php';
+  }
 
-    public function render($view, $data = [])
-    {
-        extract($data);
-        $viewPath = __DIR__ . '/../Views/' . $view . '.php';
-        if (!file_exists($viewPath)) {
-            throw new \Exception("View file {$viewPath} not found");
-        }
-
-        ob_start();
-        include $viewPath;
-        $content = ob_get_clean();
-        include __DIR__ . '/../Views/layouts/main.php';
+  public function render($view, $data = [])
+  {
+    extract($data);
+    $viewPath = __DIR__ . '/../Views/' . $view . '.php';
+    if (!file_exists($viewPath)) {
+      throw new \Exception("View file {$viewPath} not found");
     }
 
-    public function notFound()
-    {
-        http_response_code(404);
-        $this->render('errors/404');
-        exit();
-    }
+    ob_start();
+    include $viewPath;
+    $content = ob_get_clean();
+    include $this->layoutPath;
+  }
+
+  public function notFound()
+  {
+    http_response_code(404);
+    $this->render('errors/404');
+    exit();
+  }
 }
